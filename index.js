2f0cc2602299c80f479cdcedc9c4f3c51fe5e535
danielgathemia <danielgathemia01@gmail.com>
2022-09-06 22:40:49 +0300

    edit

--- index.js
+++ index.js
@@ -1,77 +1,65 @@
 //eventListeners
+ document.addEventListener('DOMContentLoaded', () =>{
+ let formInput = documrnt.getElementById(ppty-form)
+ formInput.addEventListener("submit", function (e){
+    e.preventDefault()
 
-const data = document.getElementById('ppty-form')
+    addPpty()
 
-data.addEventListener(
-    'submit', (e) =>{
-        e.preventDefault()
+    formInput.rest()
+ })
 
-        let fname =document.getElementById('name').value
-        let img =document.getElementById('img').value
-        let description =document.getElementById('description').value
-        //let list =document.getElementById('btn').value
+ function addPpty() {
+    const pptyForm = document.getElementById("ppty-form")
+    const test = document.createElement('p');
+    test.textContent = "aded ppty procedures were done at: "
+    test.style.fontWeight = "900";
 
-        sendToServer ={
-            Name: FileList,
-            imageUrl: FileList,
-            description: File
-        }
+    const title = document.createElement('h2');
+    const location = document.createElement('h3')
+    const address = document.createElement('h3');
+   
+    fetch(`http://localhost:3000/propertyData/${pptyForm.value}`)
+    .then(response => response.json())
+        .then(data => {
+    
+        
+          
+            title.innerText = data.title;
 
-        console.info(sendToServer)
+            description.innerText = data.description;
+           
+            bids.innerText = data.bids;
+           
+            });
 
-    }
+            const fName = document.getElementById("fname").value
+            const bloodTypeRadio = document.querySelector('input[name="bloodType"]:checked').value;
+            const gender = document.querySelector('input[name="gender"]:checked').value;
 
-)
-console.log('hello', data)
+            alert(`Thank you for bidding ${fName}.Your information has been registered successfully.`)
 
+        const container = document.createElement('div')
+        container.setAttribute('id', 'container')
+        
+        const p1 = document.createElement('p')
+        p1.textContent = `Full Name: ${fName}`
+        p1.style.fontWeight = "900";
 
+        const pelleven = document.createElement('p')
+        pelleven.textContent = `Donated Before: ${decision}`
+        pelleven.style.fontWeight = "900";
 
+        const ptwelve = document.createElement('p')
+        ptwelve.textContent = `Date Of initial Donation: ${date2}`
+        ptwelve.style.fontWeight = "900";
 
+        container.append(test,title,location,address,)
+        
+        const myDiv = document.getElementById("ppty-list")
+        myDiv.append(container)
+          }
 
-
-//dom render function
-function renderOneProperty(ppty){
-    //build ppty
-    let card = document.createElement('li')
-    card.className = 'card'
-    card.innerHtml = `
-    <img src="${property.imageUrl}">
-        <div class='content'>
-            <h4>${property.name}</h4>
-            <p>
-                $<span class="bid-count">${property.bid}</span> bided
-            </p>
-
-            <p>${property.description}</p>
-        </div> 
-
-        <div class="buttons">
-        <button> bid $50 </button>
-        <button> condirmed </button>
-        </div>
-    `    
-}
-
-//add property to DOM
-//document.querySelector('#ppty-list').appendChild(cards)
-
-
-//fetch request
-//get fetch for all animal resources
-function getAllProperty(){
-    fetch('http://localhost:3000/propertyData')
-    //unpackage data
-    .then(res => res.json())
-    //run the unpackaged data
-    .then(propertyData => propertyData.forEach(ppty => renderOneProperty(ppty)))
-    console.log('before fetch returns')
-}
-
-// initial render
-//get data and render our ppty to dom
-function initialize(){
-    getAllProperty()
-   // propertyData.forEach(property => renderOneProperty(property))
-}
-
-initialize()
+        
+    
+})
